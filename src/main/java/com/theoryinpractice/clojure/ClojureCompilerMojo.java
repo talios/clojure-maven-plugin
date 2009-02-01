305c3c313ae57c4c0c216ca9b6e10b3b0116c159
Mark Derricutt <mark@talios.com>
2009-02-01 14:00:14 +1300

    Initial commit

---
+++ src/main/java/com/theoryinpractice/clojure/ClojureCompilerMojo.java
@@ -0,0 +1,121 @@
+package com.theoryinpractice.clojure;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal compile
+ * @phase compile
+ */
+public class ClojureCompilerMojo extends AbstractMojo {
+
+    /**
+     * Location of the file.
+     *
+     * @parameter expression="${project.build.directory}/classes"
+     * @required
+     */
+    private File outputDirectory;
+
+    /**
+     * Location of the source files.
+     *
+     * @parameter expression="src/main/clojure"
+     * @required
+     */
+    private File srcDirectory;
+
+    /**
+     * A list of namespaces to compile
+     *
+     * @parameter
+     */
+    private String[] namespaces;
+
+    public void execute() throws MojoExecutionException {
+
+//        extendRealmClasspath();
+
+        outputDirectory.mkdirs();
+
+        Enumeration<URL> path = null;
+        try {
+            path = Thread.currentThread().getContextClassLoader().getResources("clojure");
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        String cp = srcDirectory.getPath() + File.pathSeparator + outputDirectory.getPath();
+
+        while (path.hasMoreElements()) {
+            URL url = path.nextElement();
+
+            getLog().debug(url.getPath());
+            if ("jar".equals(url.getProtocol())) {
+                cp = cp + File.pathSeparator + url.getPath().replaceFirst("file:", "").replaceFirst("jar.*", "jar");
+            }
+        }
+
+        getLog().debug("Using cp of " + cp);
+
+
+        List<String> args = new ArrayList<String>();
+        args.add("java");
+        args.add("-cp");
+        args.add(cp);
+        args.add("-Dclojure.compile.path=" + outputDirectory.getPath() + "");
+        args.add("clojure.lang.Compile");
+        for (String namespace : namespaces) {
+            args.add(namespace);
+        }
+
+        System.out.println(args);
+
+        ProcessBuilder pb = new ProcessBuilder(args);
+        pb.environment().put("path", ";");
+        pb.environment().put("path", System.getProperty("java.home"));
+
+        pb.redirectErrorStream(true);
+        try {
+            writeProcessOutput(pb.start());
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage());
+        }
+
+    }
+
+    public void writeProcessOutput(Process process) throws IOException {
+        InputStreamReader tempReader = new InputStreamReader(new BufferedInputStream(process.getInputStream()));
+        BufferedReader reader = new BufferedReader(tempReader);
+        while (true) {
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            getLog().info(line);
+        }
+    }
+
+}
