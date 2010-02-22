83b2700600959e9aa5a632673ced8451c960d6d1
Mark Derricutt <mark@talios.com>
2010-02-22 15:12:24 +1300

    Added EPL header to each source file

--- src/test/java/com/theoryinpractise/clojure/ClojureReplMojoTest.java
+++ src/test/java/com/theoryinpractise/clojure/ClojureReplMojoTest.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright (c) Mark Derricutt 2010.
+ *
+ * The use and distribution terms for this software are covered by the Eclipse Public License 1.0
+ * (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html
+ * at the root of this distribution.
+ *
+ * By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+ *
+ * You must not remove this notice, or any other, from this software.
+ */
+
 package com.theoryinpractise.clojure;
 
-import java.util.*;
-import junit.framework.*;
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ClojureReplMojoTest extends TestCase {
-    
+
 	public void testJLineAvailable() throws Exception {
 		ClojureReplMojo mojo = new ClojureReplMojo();
 		assertTrue(mojo.isJLineAvailable(Arrays.asList("test/jline-0.9.94.jar")));
--- src/test/java/com/theoryinpractise/clojure/NamespaceDiscoveryTest.java
+++ src/test/java/com/theoryinpractise/clojure/NamespaceDiscoveryTest.java
@@ -1,19 +1,30 @@
+/*
+ * Copyright (c) Mark Derricutt 2010.
+ *
+ * The use and distribution terms for this software are covered by the Eclipse Public License 1.0
+ * (http://opensource.org/licenses/eclipse-1.0.php) which can be found in the file epl-v10.html
+ * at the root of this distribution.
+ *
+ * By using this software in any fashion, you are agreeing to be bound by the terms of this license.
+ *
+ * You must not remove this notice, or any other, from this software.
+ */
+
 package com.theoryinpractise.clojure;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
-import static org.fest.assertions.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-import static org.mockito.Mockito.mock;
 
 import java.io.File;
 import java.util.List;
 
-import com.theoryinpractise.clojure.NamespaceDiscovery;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 @RunWith(Theories.class)
 public class NamespaceDiscoveryTest {
