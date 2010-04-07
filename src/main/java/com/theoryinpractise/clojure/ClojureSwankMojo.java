2e7e75c16490b0ab324689d9ae368b9543cdf18d
Nelson Morris <nmorris@nelsonmorris.net>
2010-04-07 10:57:21 +0800

    Change swank goal to use test dependencies.

--- src/main/java/com/theoryinpractise/clojure/ClojureSwankMojo.java
+++ src/main/java/com/theoryinpractise/clojure/ClojureSwankMojo.java
@@ -24,7 +24,7 @@ import java.util.List;
 /**
  * @goal swank
  * @execute phase="compile"
- * @requiresDependencyResolution compile
+ * @requiresDependencyResolution test
  */
 public class ClojureSwankMojo extends AbstractClojureCompilerMojo {
 
