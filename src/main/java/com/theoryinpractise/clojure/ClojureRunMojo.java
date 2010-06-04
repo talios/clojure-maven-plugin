e71612437eabaab7fc15ad7ef2c7d7b11a2a6845
Chas Emerick <cemerick@snowtide.com>
2010-06-04 14:18:15 +0800

    add support for classpath-loaded scripts in run mojo (@-prefixed paths) and ${clojure.script} property configuration

--- src/main/java/com/theoryinpractise/clojure/ClojureRunMojo.java
+++ src/main/java/com/theoryinpractise/clojure/ClojureRunMojo.java
@@ -16,6 +16,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,7 +31,7 @@ public class ClojureRunMojo extends AbstractClojureCompilerMojo {
     /**
      * The main clojure script to run
      *
-     * @parameter
+     * @parameter expression="${clojure.script}"
      * @required
      */
     private String script;
@@ -49,23 +50,29 @@ public class ClojureRunMojo extends AbstractClojureCompilerMojo {
      */
     private String args;
 
-    public void execute() throws MojoExecutionException {
-
-        if (script == null) {
+    /**
+     * Returns either a path to a temp file that loads all of the provided scripts,
+     * or simply returns the singular <code>script</code> String (which therefore allows
+     * for @ classpath-loading paths to be passed in as a script).
+     *
+     * If multiple scripts are defined, they must all exist; otherwise an exception is thrown.
+     */
+    private static String mergeScripts (String script, String[] scripts) throws MojoExecutionException {
+        if (script == null || script.trim().equals("")) {
             throw new MojoExecutionException("<script> is undefined");
         }
-        if (scripts != null && scripts.length == 0) {
+        if (scripts == null) {
+            return script;
+        } else if (scripts.length == 0) {
             throw new MojoExecutionException("<scripts> is defined but has no <script> entries");
         }
 
-        List<String> scriptFiles = new ArrayList<String>();
-        scriptFiles.add(script);
-        if (scripts != null) {
-            scriptFiles.addAll(Arrays.asList(scripts));
-        }
+        List<String> paths = new ArrayList<String>();
+        paths.add(script);
 
-        for (String scriptFile : scriptFiles) {
-            if (scriptFile == null || "".equals(scriptFile)) {
+        paths.addAll(Arrays.asList(scripts));
+        for (String scriptFile : paths) {
+            if (scriptFile == null || scriptFile.trim().equals("")) {
                 throw new MojoExecutionException("<script> entry cannot be empty");
             }
             if (!(new File(scriptFile).exists())) {
@@ -75,22 +82,31 @@ public class ClojureRunMojo extends AbstractClojureCompilerMojo {
 
         try {
             File testFile = File.createTempFile("run", ".clj");
-            final PrintWriter writer = new PrintWriter(new FileWriter(testFile));
+            final FileWriter writer = new FileWriter(testFile);
 
-            for (String scriptFile : scriptFiles) {
-                writer.println("(load-file \"" + scriptFile + "\")");
+            for (String scriptFile : paths) {
+                writer.write("(load-file \"" + scriptFile + "\")");
+                writer.write(System.getProperty("line.separator"));
             }
             writer.close();
+            return testFile.getPath();
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+    }
 
+    public void execute() throws MojoExecutionException {
+        String path = mergeScripts(script, scripts);
 
+        try {
             List<String> clojureArguments = new ArrayList<String>();
-            clojureArguments.add(testFile.getPath());
+            clojureArguments.add(path);
 
             if (args != null) {
                 clojureArguments.addAll(Arrays.asList(args.split(" ")));
             }
 
-            getLog().debug("Running clojure:run against " + testFile.getPath());
+            getLog().debug("Running clojure:run against " + path);
 
             callClojureWith(
                     getSourceDirectories(SourceDirectory.COMPILE),
@@ -101,6 +117,4 @@ public class ClojureRunMojo extends AbstractClojureCompilerMojo {
             throw new MojoExecutionException(e.getMessage(), e);
         }
     }
-
-
 }
