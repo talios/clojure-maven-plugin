51fa362765c053a335d091f383b13bf73d9a319a
Mark Derricutt <mark@talios.com>
2024-09-12 18:32:00 +1200

    Output human readable test results to stdout during test-with-junit.
    Fixes #61 - manually merged after a long time.

--- src/main/resources/default_test_script.clj
+++ src/main/resources/default_test_script.clj
@@ -9,17 +9,17 @@
 (def props (Properties.))
 (.load props (FileInputStream. (first *command-line-args*)))
 
-(def namespaces  (into [] 
+(def namespaces  (into []
                        (for [[key val] props
                              :when (.startsWith key "ns.")]
-                               (symbol val))))
+                         (symbol val))))
 
 (def junit (Boolean/valueOf (.get props "junit")))
 (def output-dir (.get props "outputDir"))
 (def xml-escape (Boolean/valueOf (.get props "xmlEscape")))
 
 (dorun (for [ns namespaces]
-  (require ns)))
+         (require ns)))
 
 (def escape-xml-map
   (zipmap "'<>\"&" (map #(str \& % \;) '[apos lt gt quot amp])))
@@ -30,51 +30,56 @@
 (defn xml-escaping-writer
   [writer]
   (proxy
-    [java.io.FilterWriter] [writer]
+   [java.io.FilterWriter] [writer]
     (write [text]
       (if (string? text)
         (.write writer (escape-xml text))
-        (.write writer text)))
-    ))
+        (.write writer text)))))
 
 (defn total_errors [summary]
   (+ (:error summary 0) (:fail summary 0)))
 
 (defn print-results [results]
   (println (str "Tests run: " (:test results)
-             ", Assertions: " (:pass results)
-             ", Failures: " (:fail results)
-             ", Errors: " (:error results)))
+                ", Assertions: " (:pass results)
+                ", Failures: " (:fail results)
+                ", Errors: " (:error results)))
   (if (> (total_errors results) 0)
     (println "There are test failures.")))
 
 (when-not *compile-files*
   (let [results (atom {})]
     (let [report-orig report
-          junit-report-orig junit-report]
+          junit-report-orig junit-report
+          out-orig *out*
+          test-out-orig *test-out*]
       (binding [report (fn [x] (report-orig x)
                          (swap! results (partial merge-with +)
-                           (select-keys (into {} (rest x)) [:pass :test :error :fail ])))
-                junit-report (fn [x] (junit-report-orig x)
+                                (select-keys (into {} (rest x)) [:pass :test :error :fail])))
+                junit-report (fn [x]
+                               (junit-report-orig x)
+                               (binding [*test-out* test-out-orig
+                                         *out*      out-orig]
+                                 (report-orig x))
                                (swap! results (partial merge-with +)
-                                 (select-keys (into {} (rest x)) [:pass :test :error :fail ])))]
+                                      (select-keys (into {} (rest x)) [:pass :test :error :fail])))]
         (dorun (for [ns namespaces]
-          (if junit
-            (if xml-escape
-              (do
-                (with-open [writer (FileWriter. (str output-dir "/" ns ".xml"))
-                            escaped (xml-escaping-writer writer)]
-                            (binding [*test-out* writer *out* escaped]
-                              (with-junit-output
-                                (run-tests ns)))))
-              (do
+                 (if junit
+                   (if xml-escape
+                     (do
+                       (with-open [writer (FileWriter. (str output-dir "/" ns ".xml"))
+                                   escaped (xml-escaping-writer writer)]
+                         (binding [*test-out* writer *out* escaped]
+                           (with-junit-output
+                             (run-tests ns)))))
+                     (do
                 ;;Use with-test-out to fix with-junit-output for Clojure 1.2 (See http://dev.clojure.org/jira/browse/CLJ-431)
-                (with-open [writer (FileWriter. (str output-dir "/" ns ".xml"))]
-                  (binding [*test-out* writer]
-                    (with-test-out
-                      (with-junit-output
-                        (run-tests ns)))))))
-            (run-tests ns))))
-    (shutdown-agents)
-    (print-results @results)
-    (System/exit (total_errors @results))))))
+                       (with-open [writer (FileWriter. (str output-dir "/" ns ".xml"))]
+                         (binding [*test-out* writer]
+                           (with-test-out
+                             (with-junit-output
+                               (run-tests ns)))))))
+                   (run-tests ns))))
+        (shutdown-agents)
+        (print-results @results)
+        (System/exit (total_errors @results))))))
